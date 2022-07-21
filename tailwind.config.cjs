ab9afad9c70cb0b12f9329e1be5e7823d2a5d145
Elton Souza <elton.souza1515@gmail.com>
2022-07-21 16:48:42 -0300

    feat: add breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,20 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
+    screens: {
+      sm: {'max':'480px'},
+      md: {'max':'768px'},
+      lg: {'max':'976px'},
+      xl: {'max':'1440px'}
+    },
     extend: {
-      colors:{
+      colors: {
         primary: ' #00303F',
         secondary: '#5BB0CA',
         tertiary: '#E3C232',
       }
     },
+
   },
   plugins: [],
 }
