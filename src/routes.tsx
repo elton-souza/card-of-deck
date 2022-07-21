a74c7a7f890dff4dd99194ed2f04862403402b88
Elton Souza <elton.souza1515@gmail.com>
2022-07-21 16:49:14 -0300

    refact: change component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { DeckCards } from "./pages/DeckCards";
-import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 import { UserProvider } from "./context/userContext";
 
 export function Router() {
   return (
     <UserProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/cards" element={<DeckCards />} />
       </Routes>
     </UserProvider>
